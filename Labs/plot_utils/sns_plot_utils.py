2e42f689c38ccff6ce9927571d40b32ebb9cf2e6
Jacob <kapsjacob@gmail.com>
2024-09-24 11:44:35 -0500

    lab 1

---
+++ Labs/plot_utils/sns_plot_utils.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_func_sns(plot_df, func="e^x"):
+    plt.figure(figsize=(12, 6), dpi=100)
+    sns.lineplot(data=plot_df, x="x", y="y")
+    plt.xlabel("$x$")
+    plt.ylabel(f"${func}$")
+    plt.title(f"$f(x) = {func}$")
+    plt.grid(True)
+    plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 2023FA_IEMS_351
-IEMS-351: Optimization Methods in Data Science (Fall 2023)
+# IEMS-351: Optimization Methods in Data Science (Fall 2024)
